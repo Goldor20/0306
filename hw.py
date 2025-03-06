aa4b61c34bda3c536eae1a121caf5ed85a84589b
D244TAN-01 <D244TAN-01@szeruskert.boronkay.local>
2025-03-06 08:44:37 +0100

    asd

---
+++ hw.py
@@ -0,0 +1,14 @@
+#print("Hello world")
+#a = "egy bármilyen szöveg"
+#print("szoveg")
+#print(a)
+
+
+#Változó: a mmóriában eltárolt érték
+#két szöveg összeillesztése a + jellel történik
+#sszefűzésnek (konkatenációnak) hívják
+nev = "Kovács Zalán"
+szuletesnap = "2010.01.24"
+print ("Jó napot " + nev + "!")
+print ("Jó napot", nev, "!",) # sep="" nincs szóköz
+print (12/6)
